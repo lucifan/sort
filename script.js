e92071ce1765bea68de9517853b9f2a7f4b8d45a
lucifan <fanshaowolf@163.com>
2016-05-14 22:14:20 +0800

    add button reset with eventListener

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 var numberBox = document.getElementsByClassName("number");
+var startBtn = document.getElementsByClassName("startBtn")[0];
+var resetBtn = document.getElementsByClassName("resetBtn")[0];
+var number = [];
 
-function setPosition() {
+function initial() {
+	for (var i = 0; i < 10; i++) {
+		number[i] = i+1;
+	}
+	number.sort(function() {
+		return Math.random() *  2 - 1;
+	});
 	for (var i = 0; i < numberBox.length; i++) {
+		numberBox[i].innerText = number[i];
 		numberBox[i].style.left = (i * 45) + "px";
 	}
 }
 
+function resetNumber() {
+	number.sort(function() {
+		return Math.random() *  2 - 1;
+	});
+	for (var i = 0; i < numberBox.length; i++) {
+		numberBox[i].innerText = number[i];
+	}
+}
+
 window.onload = function() {
-	setPosition();
+	initial();
+	resetBtn.addEventListener("click", resetNumber);
 }
