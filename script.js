75d214e44a304b21ce4b1c5fbe33869ccbd29cc0
lucifan <fanshaowolf@163.com>
2016-05-18 15:37:18 +0800

    start button is working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var numberBox = document.getElementsByClassName("number");
+var pointerI = document.getElementsByClassName("pointer-i")[0];
+var pointerJ = document.getElementsByClassName("pointer-j")[0];
 var startBtn = document.getElementsByClassName("startBtn")[0];
 var resetBtn = document.getElementsByClassName("resetBtn")[0];
 var number = [];
@@ -25,7 +27,56 @@ function resetNumber() {
 	}
 }
 
+function bubbleSort() {
+	var posI = 0;
+	var posJ = number.length-1;
+	pointerI.style.visibility = "visible";
+	pointerJ.style.visibility = "visible";
+	continueSort(posI, posJ);
+}
+
+function continueSort(posI, posJ) {
+	var i = posI;
+	var j = posJ;
+	for (; i < number.length; i++) {
+		for (; j > i; j--) {
+			pointerI.style.left = (15 + i * 45) + "px";
+			pointerJ.style.left = (15 + j * 45) + "px";
+			for (var k = 0; k < numberBox.length; k++) {
+				if (numberBox[k].style.left == ((j - 1) * 45) + "px") {
+					var leftBox = numberBox[k];
+				}
+				if (numberBox[k].style.left == (j * 45) + "px") {
+					var rightBox = numberBox[k];
+				}
+			}
+			if (parseInt(leftBox.innerText) >= parseInt(rightBox.innerText)) {
+				var times = 45;
+				var count = 1;
+				var left = parseInt(leftBox.style.left);
+				var right = parseInt(rightBox.style.left);
+				var move = setInterval((function(leftBox, rightBox, left, right) {
+					return function() {
+						leftBox.style.left = (left + count) + "px";
+						rightBox.style.left = (right - count) + "px";
+						count++;
+						if (count > times) {
+							clearInterval(move);
+						}
+					};
+				})(leftBox, rightBox, left, right), 50);
+				var nextMove = setTimeout(function() {
+					continueSort(i, j);
+				}, 3000);
+				return;
+			}
+		}
+		j = number.length - 1;
+	}
+}
+
 window.onload = function() {
 	initial();
 	resetBtn.addEventListener("click", resetNumber);
+	startBtn.addEventListener("click", bubbleSort);
 }
